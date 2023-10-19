14da68503905dd6798dd973c243374e46c80f7ef
letranphu <52100572@student.tdtu.edu.vn>
2023-10-19 22:51:08 +0700

    ui: main, home, sidebar

---
+++ 52100572_52100852_Source_GK/52100572_52100852_Source_GK/Login.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace _52100572_52100852_Source_GK
+{
+    public partial class Login : MaterialForm
+    {
+        private MainForm mainForm;
+        public Login()
+        {
+            InitializeComponent();
+            mainForm = new MainForm();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.LightBlue700, Primary.LightBlue900, Primary.LightBlue500, Accent.Blue700, TextShade.WHITE);
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            mainForm.Show();
+            this.Hide();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void input_password_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
