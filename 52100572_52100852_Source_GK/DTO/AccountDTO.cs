42dbf7147c25424079384f129ac70021db413456
trongtinh7727 <trongtinh7727@gmail.com>
2023-10-19 22:57:37 +0700

    feat(auth): add session_manager, account

---
+++ 52100572_52100852_Source_GK/DTO/AccountDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class AccountDTO
+    {
+        public int AccountID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+ 
+    }
+
+}
